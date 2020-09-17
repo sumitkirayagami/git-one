770ad614d5135d2d11053998bcdffd8335175d7d
iamsumitk <sumitkumar141099ash@gmail.com>
2020-09-17 23:43:03 +0530

    third console statement in line 10 is added

--- feature-b.js
+++ feature-b.js
@@ -1,4 +1,10 @@
 console.log("feature-b");
 
 
-console.log("currently branch is feature-a at the time of changing"0);
+console.log("currently branch is feature-a at the time of changing");
+
+
+console.log("currently branch is feature-b at the time of changing");
+
+
+console.log("currently branch is feature-a at the time of changing");
