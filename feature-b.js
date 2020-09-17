2ac6d91347a2540e3fea4f2ec0f826ac3c1eb3c1
iamsumitk <sumitkumar141099ash@gmail.com>
2020-09-17 23:45:12 +0530

    write log statement with branch master

---
+++ feature-b.js
@@ -0,0 +1,13 @@
+console.log("feature-b");
+
+
+console.log("currently branch is feature-a at the time of changing");
+
+
+console.log("currently branch is feature-b at the time of changing");
+
+
+console.log("currently branch is feature-a at the time of changing");
+
+
+console.log("currently branch is master at the time of changing");
